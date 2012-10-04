fa9c0aac9c68aaa4e08a5f2356a3a34992ee67e6
Amit Singh <amit1093@gmail.com>
2012-10-04 21:17:45 +0530

    Gymkhanan redesing:

--- ugacademics/public_html/query_log.sql
+++
@@ -1,7 +0,0 @@
-/*!40019 SET @@session.max_insert_delayed_threads=0*/;
-/*!50003 SET @OLD_COMPLETION_TYPE=@@COMPLETION_TYPE,COMPLETION_TYPE=0*/;
-DELIMITER /*!*/;
-DELIMITER ;
-# End of log file
-ROLLBACK /* added by mysqlbinlog */;
-/*!50003 SET COMPLETION_TYPE=@OLD_COMPLETION_TYPE*/;
